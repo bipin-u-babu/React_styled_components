7e203fff21b45e2fd63b86ef20fcfcffeece04a3
bipinbabu <bipin.u.babu@gmail.com>
2019-11-18 10:11:30 +0530

    Added basic nav bar and installed styled-components dependency

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Switch, Route } from "react-router-dom";
+
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Details from "./components/Details";
+import Navbar from "./components/Navbar";
+import Error from "./components/Error";
+
+
 
 function App() {
   return (
-    <div className = "container">
-      <div className = "row">
-        
-      </div>
-      </div>
+    <React.Fragment>
+      <Navbar/>
+      <Switch>
+        <Route exact path = "/" component = {ProductList}/>
+        <Route path = "/details" component = {Details}/>
+        <Route path = "/cart" component = {Cart}/>
+        <Route  component = {Error}/>
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5 bg-primary">
+                <Link to='/'>
+                    <img src={logo} alt='store' className='navbar-brand' />
+                </Link>
+
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">Product</Link>
+                    </li>
+                </ul>
+
+                <Link to = "/cart" className= "ml-auto">
+                    <button>
+                        <i className = "fas fa-cart-plus"/>My cart
+                    </button>
+                </Link>
+            </nav>
+        )
+    }
+}
